f0ccd8d8ec4aa075070df8e8ed30aff6a984ddaf
= <=>
2024-02-12 23:22:00 +0530

    dynamic slug page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,66 @@
-
-export default function Home() {
-  return (
-  <>
-
-  </>
-  );
+import { FunctionComponent } from 'react';
+import dynamic from "next/dynamic";
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link';
+interface pageProps {
+   
 }
+ 
+const page: FunctionComponent<pageProps> = () => {
+
+    const blogDir  = "blogs";
+
+  const files = fs.readdirSync(path.join(blogDir));
+
+  const blogs =  files.map(filename =>{
+    
+    const fileContent = fs.readFileSync(path.join(blogDir, filename), 'utf-8')
+
+
+    const {data: frontMatter} = matter(fileContent);
+    return {
+      meta: frontMatter,
+      slug: filename.replace('.mdx', '')
+    }
+
+  })
+
+  // console.log(blogs)
+
+    return (  <main className='flex flex-col'>
+    <h1 className='text-3xl font-bold'>
+      My Next.Js Blog Site
+    </h1>
+
+    <section className='py-10'>
+    <h2 className='text-2xl font-blod'>
+      Latest Blogs
+    </h2>
+
+    <div className='py-2'>
+      {blogs.map(blog =>(
+        <Link href={'/blogs/' + blog.slug} passHref key={blog.slug}>
+          <div className='py-2 flex justify-between align-middle gap-2'>
+            <div>
+              <h3 className='text-lg font-blod'>
+                {blog.meta.title}
+              </h3>
+              <div>
+              
+                <p className='text-gray-400'>{blog.meta.description}</p>
+              </div>
+              <div className='my-auto text-gray-400'>
+                <p>{blog.meta.date}</p>
+              </div>
+             </div> 
+          </div>
+        </Link>
+      ))}
+    </div>
+    </section>
+   </main> );
+}
+
+export default dynamic (() => Promise.resolve(page), {ssr: false})
